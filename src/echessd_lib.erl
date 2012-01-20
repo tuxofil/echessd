68dade7fb8fd6f5ed6f9edc28fc6dd47ad05c92c
Aleksey Morarash <aleksey.morarash@gmail.com>
2012-01-21 01:02:05 +0200

    added _lib:read_file/1::string() fun

--- src/echessd_lib.erl
+++ src/echessd_lib.erl
@@ -1,7 +1,18 @@
 -module(echessd_lib).
 
--export([ip2str/1]).
+-export([ip2str/1, read_file/1]).
 
 ip2str({A, B, C, D}) ->
     io_lib:format("~B.~B.~B.~B", [A, B, C, D]).
+
+read_file(Filename) ->
+    case file:read_file(Filename) of
+        {ok, Binary} ->
+            binary_to_list(Binary);
+        {error, Reason} ->
+            echessd_log:err(
+              "Unable to read ~9999p: ~9999p",
+              [Filename, Reason]),
+            throw(Reason)
+    end.
 
