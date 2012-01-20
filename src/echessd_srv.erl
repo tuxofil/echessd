cd64b430ed73203f993fa3ec38e5cfa82e84cb91
Aleksey Morarash <aleksey.morarash@gmail.com>
2012-01-21 01:06:20 +0200

    now server is able to show test table

--- src/echessd_srv.erl
+++ src/echessd_srv.erl
@@ -34,9 +34,24 @@ loop(Req) ->
         end,
     Method = Req:get(method),
     Query = Req:parse_qs(),
+    echessd_log:debug("Query=~9999p", [Query]),
     if Method == ?HTTP_GET ->
             "/" ++ Path = Req:get(path),
             case Path of
+                "table" ->
+                    Game0 = echessd_game:new(),
+                    Moves = string:tokens(proplists:get_value("moves", Query), ","),
+                    Game =
+                        lists:foldl(
+                          fun(Move, Acc) ->
+                                  {T, _} = echessd_game:move(Acc, Move),
+                                  T
+                          end, Game0, Moves),
+                    Content =
+                        echessd_lib:read_file("www/header.html") ++
+                        echessd_html:table(Game) ++
+                        echessd_lib:read_file("www/footer.html"),
+                    Req:ok({"text/html", Content});
                 _Other ->
                     %% fixme: following behaviour is only for debug purposes
                     Req:serve_file(Path, "www")
@@ -48,4 +63,15 @@ loop(Req) ->
 %% ----------------------------------------------------------------------
 %% Internal functions
 %% ----------------------------------------------------------------------
+
+%% @doc Replaces all matched by Regexp text in String by Replacement
+%% @spec str_replace(String, Regexp, Replacement) -> NewString
+%%     String = Regexp = Replacement = NewString = string()
+str_replace(String, Regexp, Replacement)
+  when is_list(String), is_list(Regexp), is_list(Replacement) ->
+    try re:replace( String, Regexp, Replacement, [global, {return, list}]) of
+        Result -> Result
+    catch
+        _:_ -> String
+    end.
 
