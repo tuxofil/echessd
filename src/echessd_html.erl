bde9e7494688efb67de8e009672088d586adf183
Aleksey Morarash <aleksey.morarash@gmail.com>
2012-01-21 20:33:03 +0200

    group functions in '_html' mod

--- src/echessd_html.erl
+++ src/echessd_html.erl
@@ -1,9 +1,6 @@
 -module(echessd_html).
 
--export([header/2, footer/1,
-         h1/1,
-         table/1, figure/1,
-         login/0,
+-export([login/0,
          register/0,
          error/1,
          eaccess/0,
@@ -16,6 +13,130 @@
         ]).
 
 -include("echessd.hrl").
+
+%% ----------------------------------------------------------------------
+%% API functions
+%% ----------------------------------------------------------------------
+
+login() ->
+    header("echessd - Login", []) ++
+        h1("echessd login") ++
+        navig_links([{"?goto=register", "Register new user"}]) ++
+        "<form method=post>"
+        "<input name=action type=hidden value=login>"
+        "Login:    <input name=username type=text><br>"
+        "Password: <input name=password type=password><br>"
+        "<input type=submit value='Login'>"
+        "</form>" ++
+        footer([]).
+
+register() ->
+    header("echessd - Register new user", []) ++
+        h1("echessd register form") ++
+        navig_links([{"?goto=login", "Return to login form"}]) ++
+        "<form method=post>"
+        "<input name=action type=hidden value=register>"
+        "Login:    <input name=regusername type=text><br>"
+        "Password: <input name=regpassword1 type=password><br>"
+        "Confirm password: <input name=regpassword2 type=password><br>"
+        "<input type=submit value='Register'>"
+        "</form>" ++
+        footer([]).
+
+error(String) ->
+    header("echessd - Error", []) ++
+        h1("echessd error") ++
+        "<div class=error>" ++ String ++ "</div>"
+        "<br>" ++
+        navig_links([{"javascript: history.back();", "Back"}]) ++
+        footer([]).
+
+eaccess() -> ?MODULE:error("ACCESS DENIED").
+
+home() ->
+    header("echessd - Home", []) ++
+        h1("Logged in as \"" ++ get(username) ++ "\"") ++
+        navigation() ++
+        "" ++
+        footer([]).
+
+users() ->
+    {ok, Users0} = echessd_user:list(),
+    Users = lists:usort(Users0) -- get(username),
+    header("echessd - Users list", []) ++
+        h1("Users list") ++
+        navigation() ++
+        "<br>" ++
+        string:join(
+          lists:map(
+            fun(User) ->
+                    "*&nbsp;<a href='?goto=user&name=" ++ User ++ "'>" ++
+                        User ++ "</a>"
+            end, Users), "<br>") ++
+        footer([]).
+
+user() ->
+    Iam = get(username),
+    case proplists:get_value("name", get(query_proplist)) of
+        Iam -> home();
+        User ->
+            case echessd_user:getprops(User) of
+                {ok, UserInfo} ->
+                    user(User, UserInfo);
+                {error, Reason} ->
+                    ?MODULE:error(
+                       io_lib:format(
+                         "Unable to fetch user ~9999p properties:"
+                         "<br><tt>~p</tt>", [User, Reason]))
+            end
+    end.
+user(User, UserInfo) ->
+    header("echessd - User '" ++ User ++ "'", []) ++
+        h1("User '" ++ User ++ "'") ++
+        navigation() ++
+        "<br>" ++
+        string:join(
+          lists:flatmap(
+            fun({created, Time}) ->
+                    ["<b>Registered:</b> " ++ echessd_lib:timestamp(Time)];
+               (_) ->
+                    []
+            end, UserInfo), "<br>") ++
+        "<br>[&nbsp;&nbsp;]" ++
+        footer([]).
+
+game(GameID) ->
+    header("echessd - Game", []) ++
+        h1("Logged in as " ++ get(username)) ++
+        navigation() ++
+        "" ++
+        footer([]).
+
+test_table() ->
+    Game0 = echessd_game:new(?GAME_CLASSIC),
+    Moves = string:tokens(proplists:get_value("moves", get(query_proplist), ""), ","),
+    Game =
+        lists:foldl(
+          fun(Move, Acc) ->
+                  {T, _} = echessd_game:move(Acc, Move),
+                  T
+          end, Game0, Moves),
+    header("echessd - Test table", []) ++
+        h1("Test table") ++
+        navigation() ++
+        table(Game) ++
+        footer([]).
+
+notyet() ->
+    header("echessd - Under construction", []) ++
+        h1("Not implemented yet") ++
+        "" ++
+        navig_links([{"javascript: history.back();", "Back"}]) ++
+        footer([]).
+
+%% ----------------------------------------------------------------------
+%% Internal functions
+%% ----------------------------------------------------------------------
 
 header(Title, _Options) ->
     "<html>\n\n"
@@ -135,120 +256,4 @@ navigation() ->
                 ?SECTION_TEST]] ++
           [{"?action=exit", "Logout"}],
       section_caption(echessd_session:get_val(section))).
-
-login() ->
-    header("echessd - Login", []) ++
-        h1("echessd login") ++
-        navig_links([{"?goto=register", "Register new user"}]) ++
-        "<form method=post>"
-        "<input name=action type=hidden value=login>"
-        "Login:    <input name=username type=text><br>"
-        "Password: <input name=password type=password><br>"
-        "<input type=submit value='Login'>"
-        "</form>" ++
-        footer([]).
-
-register() ->
-    header("echessd - Register new user", []) ++
-        h1("echessd register form") ++
-        navig_links([{"?goto=login", "Return to login form"}]) ++
-        "<form method=post>"
-        "<input name=action type=hidden value=register>"
-        "Login:    <input name=regusername type=text><br>"
-        "Password: <input name=regpassword1 type=password><br>"
-        "Confirm password: <input name=regpassword2 type=password><br>"
-        "<input type=submit value='Register'>"
-        "</form>" ++
-        footer([]).
-
-error(String) ->
-    header("echessd - Error", []) ++
-        h1("echessd error") ++
-        "<div class=error>" ++ String ++ "</div>"
-        "<br>" ++
-        navig_links([{"javascript: history.back();", "Back"}]) ++
-        footer([]).
-
-eaccess() -> ?MODULE:error("ACCESS DENIED").
-
-home() ->
-    header("echessd - Home", []) ++
-        h1("Logged in as \"" ++ get(username) ++ "\"") ++
-        navigation() ++
-        "" ++
-        footer([]).
-
-users() ->
-    {ok, Users0} = echessd_user:list(),
-    Users = lists:usort(Users0) -- get(username),
-    header("echessd - Users list", []) ++
-        h1("Users list") ++
-        navigation() ++
-        "<br>" ++
-        string:join(
-          lists:map(
-            fun(User) ->
-                    "*&nbsp;<a href='?goto=user&name=" ++ User ++ "'>" ++
-                        User ++ "</a>"
-            end, Users), "<br>") ++
-        footer([]).
-
-user() ->
-    Iam = get(username),
-    case proplists:get_value("name", get(query_proplist)) of
-        Iam -> home();
-        User ->
-            case echessd_user:getprops(User) of
-                {ok, UserInfo} ->
-                    user(User, UserInfo);
-                {error, Reason} ->
-                    ?MODULE:error(
-                       io_lib:format(
-                         "Unable to fetch user ~9999p properties:"
-                         "<br><tt>~p</tt>", [User, Reason]))
-            end
-    end.
-user(User, UserInfo) ->
-    header("echessd - User '" ++ User ++ "'", []) ++
-        h1("User '" ++ User ++ "'") ++
-        navigation() ++
-        "<br>" ++
-        string:join(
-          lists:flatmap(
-            fun({created, Time}) ->
-                    ["<b>Registered:</b> " ++ echessd_lib:timestamp(Time)];
-               (_) ->
-                    []
-            end, UserInfo), "<br>") ++
-        "<br>[&nbsp;&nbsp;]" ++
-        footer([]).
-
-game(GameID) ->
-    header("echessd - Game", []) ++
-        h1("Logged in as " ++ get(username)) ++
-        navigation() ++
-        "" ++
-        footer([]).
-
-test_table() ->
-    Game0 = echessd_game:new(?GAME_CLASSIC),
-    Moves = string:tokens(proplists:get_value("moves", get(query_proplist), ""), ","),
-    Game =
-        lists:foldl(
-          fun(Move, Acc) ->
-                  {T, _} = echessd_game:move(Acc, Move),
-                  T
-          end, Game0, Moves),
-    header("echessd - Test table", []) ++
-        h1("Test table") ++
-        navigation() ++
-        table(Game) ++
-        footer([]).
-
-notyet() ->
-    header("echessd - Under construction", []) ++
-        h1("Not implemented yet") ++
-        "" ++
-        navig_links([{"javascript: history.back();", "Back"}]) ++
-        footer([]).
 
