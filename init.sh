39757335e5a666a6612e47f30bf8718b59f305b2
Aleksey Morarash <aleksey.morarash@gmail.com>
2012-01-24 13:47:41 +0200

    scripts fixes

--- init.sh
+++ init.sh
@@ -9,11 +9,13 @@
 ###               Warning: all existing data will be lost!
 ###-------------------------------------------------------------------
 
+set -e
+mkdir -p ./tmp
 exec erl -sname "echessd" \
     -boot start_sasl \
     -noshell -noinput \
-    -pa ebin \
-    -mnesia dir \"tmp/mnesia\" \
+    -pa ./ebin \
+    -mnesia dir \"./tmp/mnesia\" \
     -s echessd_db init \
     -s init stop
 
--- run.sh
+++ run.sh
@@ -12,10 +12,10 @@
 exec erl -sname "echessd" \
     -boot start_sasl \
     -noshell -noinput \
-    -pa ebin \
-    -pa contrib/mochiweb/ebin \
-    -mnesia dir \"tmp/mnesia\" \
-    -echessd_config echessd.conf \
+    -pa ./ebin \
+    -pa ./contrib/mochiweb/ebin \
+    -mnesia dir \"./tmp/mnesia\" \
+    -echessd_config ./echessd.conf \
     -s mnesia \
     -s echessd
 
