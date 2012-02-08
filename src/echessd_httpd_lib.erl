7233ca3570351a7c7b5ca5e5d1c01b303f2c68ad
Aleksey Morarash <aleksey.morarash@gmail.com>
2012-02-08 20:08:58 +0200

    document funs from '_httpd_lib' mod

--- src/echessd_httpd_lib.erl
+++ src/echessd_httpd_lib.erl
@@ -16,9 +16,15 @@
 %% API functions
 %% ----------------------------------------------------------------------
 
-reset_extra_headers() ->
-    erase(extra_headers), ok.
-
+%% @doc While generating HTML page sometimes we need to send
+%%      additional HTTP headers before page content.
+%%      This function will add such header data to process
+%%      dictionary and after page get generated, this
+%%      headers will be sent to client before the page.
+%% @spec add_extra_headers(ExtraHeaders) -> ok
+%%     ExtraHeaders = [{Key, Value}],
+%%     Key = string(),
+%%     Value = string()
 add_extra_headers(ExtraHeaders) ->
     put(extra_headers,
         case get(extra_headers) of
@@ -27,11 +33,25 @@ add_extra_headers(ExtraHeaders) ->
             _ -> []
         end ++ ExtraHeaders), ok.
 
+%% @doc Fetches all extra HTTP headers, saved in process
+%%      dictionary by calls to add_extra_headers/1 fun.
+%% @see add_extra_headers/1
+%% @spec get_extra_headers() -> ExtraHeaders
+%%     ExtraHeaders = [{Key, Value}],
+%%     Key = string(),
+%%     Value = string()
 get_extra_headers() ->
     case get(extra_headers) of
         [_ | _] = Headers -> Headers;
         _ -> []
     end.
+
+%% @doc Removes from process dictionary all extra HTTP headers
+%%      added earlier by add_extra_headers/1 fun calls.
+%% @see add_extra_headers/1
+%% @spec reset_extra_headers() -> ok
+reset_extra_headers() ->
+    erase(extra_headers), ok.
 
 %% ----------------------------------------------------------------------
 %% Internal functions
