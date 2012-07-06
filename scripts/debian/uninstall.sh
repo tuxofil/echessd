b69c134af70841d33583b6dd62b3c9bed58612ec
Aleksey Morarash <aleksey.morarash@gmail.com>
2012-07-06 22:34:15 +0300

    uninstall script fix

--- scripts/debian/uninstall.sh
+++ scripts/debian/uninstall.sh
@@ -8,9 +8,9 @@ deluser echessd || :
 rm -rvf -- \
     /etc/echessd.conf \
     /etc/init.d/echessd \
-    /usr/bin/echessd* \
     /usr/lib/erlang/lib/echessd* \
     /usr/share/echessd \
+    /usr/share/doc/echessd \
     /var/lib/echessd \
     /var/log/echessd \
     /var/run/echessd
